c9b50faa201104c142ccac43c06efa684ff4c72f
yurkenchik <yura.ilchshyn@gmail.com>
2024-03-05 10:42:40 +0200

    remade with binary search

--- lab_1/lab1_lev3_var1.py
+++ lab_1/lab1_lev3_var1.py
@@ -1,5 +1,5 @@
 
-def diag_up(row_idx, col_idx, rows_array_length, columns_array_lenght, arr, result_arr):
+def move_diagonally_up(row_idx, col_idx, rows_array_length, columns_array_lenght, arr, result_arr):
     while 0 < row_idx <= rows_array_length - 1 and 0 <= col_idx <= columns_array_lenght - 2:
         row_idx -= 1
         col_idx += 1
@@ -7,7 +7,7 @@ def diag_up(row_idx, col_idx, rows_array_length, columns_array_lenght, arr, resu
         result_arr.append(curr_val)
     return row_idx, col_idx
 
-def diag_down(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr):
+def move_diagonally_down(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr):
     while 0 <= row_idx <= rows_array_lenght - 2 and 0 < col_idx <= columns_array_lenght - 1:
         row_idx += 1
         col_idx -= 1
@@ -48,19 +48,19 @@ def arr_zigzag_traversed(arr, rows_array_lenght, columns_array_lenght):
 
         elif row_idx == rows_array_lenght - 1 and 0 < col_idx <= columns_array_lenght - 1:
             row_idx, col_idx = move_left(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
-            row_idx, col_idx = diag_up(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
+            row_idx, col_idx = move_diagonally_up(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
 
         elif 0 < row_idx <= rows_array_lenght - 1 and col_idx == columns_array_lenght - 1:
             row_idx, col_idx = move_up(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
-            row_idx, col_idx = diag_down(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
+            row_idx, col_idx = move_diagonally_down(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
 
         elif 0 < row_idx <= rows_array_lenght - 1 and col_idx == 0:
             row_idx, col_idx = move_up(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
-            row_idx, col_idx = diag_up(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
+            row_idx, col_idx = move_diagonally_up(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
 
         elif row_idx == 0:
             row_idx, col_idx = move_left(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
-            row_idx, col_idx = diag_down(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
+            row_idx, col_idx = move_diagonally_down(row_idx, col_idx, rows_array_lenght, columns_array_lenght, arr, result_arr)
 
 test_array = [[1, 2, 3],
               [4, 5, 6],
